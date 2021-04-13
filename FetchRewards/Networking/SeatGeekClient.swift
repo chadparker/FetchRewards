f20b1e721b65b8877d372b81c93f17a092f19fbc
Chad Parker <chad@chadparker.com>
2021-04-13 10:47:22 -0700

    Implement SeatGeekClient & NetworkError

--- FetchRewards/Networking/SeatGeekClient.swift
+++ FetchRewards/Networking/SeatGeekClient.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+enum NetworkError : Error {
+    case dataTask(Error)
+    case noData
+    case parseError(Error)
+}
+
+class SeatGeekClient {
+    
+    let baseURL = URL(string: "https://api.seatgeek.com/2/")!
+    
+    func fetchEvents(completion: @escaping (Result<[Event], NetworkError>) -> Void) {
+        
+        let url = baseURL.appendingPathComponent("events")
+        var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)!
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Secrets.clientID),
+            URLQueryItem(name: "client_secret", value: Secrets.clientSecret),
+        ]
+        
+        URLSession.shared.dataTask(with: urlComponents.url!) { data, response, error in
+            guard error == nil else {
+                DispatchQueue.main.async { completion(.failure(.dataTask(error!))) }
+                return
+            }
+            guard let data = data else {
+                DispatchQueue.main.async { completion(.failure(.noData)) }
+                return
+            }
+            do {
+                let eventsResponse = try Event.decoder.decode(EventsResponse.self, from: data)
+                DispatchQueue.main.async { completion(.success(eventsResponse.events)) }
+            } catch {
+                DispatchQueue.main.async { completion(.failure(.parseError(error))) }
+            }
+        }.resume()
+    }
+}
