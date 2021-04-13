5d72a1af6907dfb14cdc34b85c54f1ef676c010d
Chad Parker <chad@chadparker.com>
2021-04-13 11:31:47 -0700

    Trigger search when SearchBar text changes

--- FetchRewards/View Controllers/MainViewController.swift
+++ FetchRewards/View Controllers/MainViewController.swift
@@ -11,7 +11,28 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
     
+    let seatGeekClient = SeatGeekClient()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+}
+
+extension MainViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let query = searchText
+        seatGeekClient.fetchEventsMatchingQuery(query) { result in
+            switch result {
+            case .success(let events):
+                print("✅\(events.count) events fetched")
+            case .failure(let networkError):
+                print("🛑Error: \(networkError)")
+            }
+        }
     }
 }
