7ae5a382ddc52a427d6217624e91bb080418c3cd
Chad Parker <chad@chadparker.com>
2021-04-13 10:48:11 -0700

    Add `testFetchEvents()` unit test to test fetching

--- FetchRewardsTests/FetchRewardsTests.swift
+++ FetchRewardsTests/FetchRewardsTests.swift
@@ -6,7 +6,24 @@
 //
 
 import XCTest
+@testable import FetchRewards
 
 class FetchRewardsTests: XCTestCase {
+    
+    var seatGeekClient = SeatGeekClient()
 
+    func testFetchEvents() throws {
+        let expectation = self.expectation(description: "Waiting for SeatGeekClient")
+        seatGeekClient.fetchEvents { result in
+            switch result {
+            case .success(let events):
+                print("✅\(events.count) events fetched")
+            case .failure(let networkError):
+                print("🛑Error: \(networkError)")
+                XCTFail()
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 5)
+    }
 }
